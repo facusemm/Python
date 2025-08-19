8fda28679fff9e8edaa2713c72c90e99100f9a52
fsemmartin <fsemmartin@frba.utn.edu.ar>
2025-08-19 12:49:41 -0300

    Charapter 4 completed

---
+++ python_crash_course_by_eric_matthes/charapter_4/(4-13)buffet.py
@@ -0,0 +1,11 @@
+print("Original Menu:")
+foods = ("Pizza", "Milanesa", "Empanada", "Pasta", "Flan")
+for food in foods:
+    print(food)
+
+#foods[0] = "Bondiola"
+
+print("\nNew Menu:")
+foods = ("Bondiola", "Matambre", "Empanada", "Pasta", "Flan")
+for food in foods:
+    print(food)
